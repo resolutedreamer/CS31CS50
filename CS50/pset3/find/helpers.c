80639a212a503ac95378299f2f2c4700d5674680
asian701129 <asian701129@ucla.edu>
2014-02-17 08:50:43 -0800

    Search and Sort Complete?
    Need to test them.

--- CS50/pset3/find/helpers.c
+++ CS50/pset3/find/helpers.c
@@ -17,6 +17,11 @@
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
+	for (int i = 0; i < n; i++)
+	{
+		if (values[i] == value)
+			return true;
+	}
     return false;
 }
 
@@ -26,5 +31,29 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+	
+	for (int i = 0; i < n; i++)
+    {
+        int smallestnumber = 50, smallestnumberindex = 0;
+        int k = i;
+        while (k < n)
+        {
+            if (values[k] < smallestnumber)
+            {
+                smallestnumber = values[k];
+                smallestnumberindex = k;
+            }
+            k++;
+        }
+        printf("%dth iteration\n %d is smallest\n",i, smallestnumber);
+        int temp1 = values[i];
+        int temp2 = smallestnumber;
+        values[i] = temp2;
+        values[smallestnumberindex] = temp1;
+        k = 0;
+        smallestnumber = 50;
+    }
+    
+	
     return;
 }
